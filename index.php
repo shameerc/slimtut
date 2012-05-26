a072cb02f7ca9ca7b48820263e6b76ae6be89e74
Shameer <shameer806@gmail.com>
2012-05-26 18:41:40 +0530

    updated get route

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+ini_set('dsiaplay_errors', true);
+error_reporting(E_ALL);
 /**
  * Step 1: Require the Slim PHP 5 Framework
  *
@@ -44,22 +47,28 @@ $app->post('/book',function() use($app,$db){
 $app->get('/books',function() use ($app,$db){
     $app->response()->header('Content-Type', 'application/json');
     $users = array();
-    foreach ($db->books as $user) {
+    foreach ($db->books as $book) {
         $users[]  = array(
-                'id' => $user['id'],
-                'title' => $user['title'],
-                'author' => $user['author'],
-                'summary' => $user['summary']
+                'id' => $book['id'],
+                'title' => $book['title'],
+                'author' => $book['author'],
+                'summary' => $book['summary']
             );
     }
     echo json_encode($users);
-
 });
 
 $app->get('/book/:id',function($id) use($app,$db){
     $app->response()->header('Content-Type', 'application/json');
     $book = $db->books()->where('id',$id);
-    print_r($book->fetch());
+    if($data = $book->fetch()){
+        echo json_encode(array(
+                'id' => $data['id'],
+                'title' => $data['title'],
+                'author' => $data['author'],
+                'summary' => $data['summary']
+            ));
+    }
 });
 
 $app->delete('/book/:id',function($id) use($app,$db){
@@ -95,6 +104,11 @@ $app->put('/book/:id',function($id) use($app,$db){
                 'message' => "Book id $id does not exist"
             ));
     }
+});
+
+$app->notFound(function() use($app){
+    $app->response()->header('Content-Type', 'application/json');
+    echo json_encode(array('status' => 404, 'message' => 'Not supported'));
 });
 
 $app->run();
